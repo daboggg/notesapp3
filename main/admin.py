5db792d74abcab02d6d99fbd17022f990e43b3d3
daboggg <vzinin@list.ru>
2021-08-10 10:02:07 +0300

    rename Category to NotesCateory

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Category)
+admin.site.register(NotesCategory)
 admin.site.register(Note)
--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Category(models.Model):
+class NotesCategory(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
@@ -21,7 +21,7 @@ class Note(models.Model):
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Опубликован?')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT,verbose_name='Категория')
+    category = models.ForeignKey(NotesCategory, on_delete=models.PROTECT, verbose_name='Категория')
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='User')
 
     def __str__(self):
