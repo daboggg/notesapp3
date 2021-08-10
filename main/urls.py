70aa12d9838943f0712537d8b6fcbf86483707c3
daboggg <vzinin@list.ru>
2021-08-10 14:10:46 +0300

    add view Notes
    get all notes by user

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ app_name = 'main'
 urlpatterns =[
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
+    path('notes/', Notes.as_view(), name='notes'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('', index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,14 +2,38 @@ from django.contrib.auth import logout, login
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from main.form import LoginUserForm, RegisterUserForm
+from main.models import Note
 
 
 def index(request):
     return render(request, 'main/index.html', {'title': 'Главная страница'})
 
+
+# def notes(request):
+#     return render(request, 'main/notes.html', {'title': 'Заметки'})
+
+
+# Все записки по авторизованому пользователю
+class Notes(ListView):
+    model = Note
+    context_object_name = 'notes'
+    template_name = 'main/notes.html'
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
+
+
+    def get_context_data(self, **kwargs):
+        # print(dir(self.request.user))
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записки'
+        return context
+
+
+# Регистрация и авторизация
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'main/login.html'
