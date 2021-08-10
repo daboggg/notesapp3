e2d4785aad78cca864cbcfa412b592a9e3e71803
daboggg <vzinin@list.ru>
2021-08-10 09:26:13 +0300

    add model Note

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -10,4 +11,23 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100, db_index=True, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    content = models.TextField(blank=True, verbose_name='Текст записки')
+    image = models.ImageField(upload_to='img/%Y/%m/%d/', verbose_name='Изображение')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликован?')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,verbose_name='Категория')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='User')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Записка'
+        verbose_name_plural = 'Записки'
 
