7c025c6ad6ac2f495f947d635db99f1aba92d48b
daboggg <vzinin@list.ru>
2021-08-11 10:02:25 +0300

    Add templatetag get_note_categories

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(NotesCategory)
+class NotesCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(NotesCategory, NotesCategoryAdmin)
 admin.site.register(Note)
