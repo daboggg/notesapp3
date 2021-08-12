c9d4d983f1b4c9aafa3c8b06228b53f61ee070e4
daboggg <vzinin@list.ru>
2021-08-12 10:34:55 +0300

    Дабавил отображение адаптивных карточек bootstrap

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.views import serve
 from django.urls import path, include
@@ -26,4 +27,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
+    # urlpatterns.append(path('static/<path:path>', never_cache(serve)))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
