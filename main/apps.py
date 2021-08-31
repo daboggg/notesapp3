596987712dbdf392c3af7581ae2b86db774ba101
daboggg <vzinin@list.ru>
2021-08-31 16:08:26 +0300

    create логику reminder

--- main/apps.py
+++ main/apps.py
@@ -1,7 +1,15 @@
 from django.apps import AppConfig
+from django.dispatch import Signal, receiver
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
     verbose_name = 'Заметки и напоминалки'
+
+
+# test_sngl = Signal(providing_args=['test'])
+#
+# @receiver(test_sngl)
+# def test_sngl_dispatcher(sender, **kwargs):
+#     pass
--- main/management/commands/remind_command.py
+++ main/management/commands/remind_command.py
@@ -1,5 +1,13 @@
+from django.core.mail import send_mail, EmailMessage, EmailMultiAlternatives
 from django.core.management import BaseCommand
 from datetime import datetime
+
+from django.template.loader import render_to_string
+
+from main.utils import delete_reminder
+from notesapp import settings
+
+from main.models import Reminder
 
 
 class Command(BaseCommand):
@@ -9,6 +17,44 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         id = kwargs.get('id')
-        with open('two.txt', 'a') as file:
-            file.write(f'{id} : {datetime.now()}')
-        print(id)
+        try:
+            r = Reminder.objects.get(pk=id)
+        except Reminder.DoesNotExist:
+            return
+
+        context={'r': r}
+        s = render_to_string('parts/mail.html', context)
+        try:
+            em = EmailMultiAlternatives(subject=r.title, body=r.content,from_email=settings.DEFAULT_FROM_EМAIL, to=[r.user.email])
+            em.attach_alternative(s, 'text/html')
+            em.send()
+        except Exception as e:
+            with open('two.txt', 'a') as file:
+                file.write(str(e))
+
+        if r.is_once:
+            delete_reminder(id)
+            r.delete()
+
+
+        # ml = send_mail(
+        #     r.title,
+        #     r.content,
+        #     settings.DEFAULT_FROM_EМAIL,
+        #     [r.user.email],
+        #     fail_silently=False,
+        #     html_message=s
+        # )
+
+
+
+        # with open('two.txt', 'a') as file:
+        #     file.write(f'{str(ml)}\n')
+
+        # if ml:
+        #     if r.is_once:
+        #         delete_reminder(id)
+        #         r.delete()
+
+
+
